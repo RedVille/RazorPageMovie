8a34845a59c1b85298cb59fdfbb6f367537c8bee
yanet <yanet@REDVILLE>
2023-11-04 10:53:27 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Timers;
+
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; }
+        //Las etiquetas se utilizan para dar un formato específico al tipo de dato solicitado
+        [Display(Name = "Realese Date")]
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+
+namespace RazorPageMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext1(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPageMovieContext1>>()))
+            {
+                // En caso de contexto nulo, se envía un mensaje de error
+                if(context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                if(context.Movie.Any())
+                {
+                    return;
+                }
+
+                //Herencia
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry met Sally",
+                        ReleaseDate = DateTime.Parse("1989-02-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-03-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1989-02-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Río Bravo",
+                        ReleaseDate = DateTime.Parse("1959-04-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                    }
+                 );
+                context.SaveChanges();
+            }
+        }
+    }
+}
